8051885e30f4760c5560f45a74cd34c1117c28a6
sushilydv-dev <sushilyadav292001@gmail.com>
2024-07-16 23:17:18 -0700

    first commit

---
+++ passop/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+               
+
+                <span>Passer</span>
+
+
+            </div >
+            <div className='flex justify-center items-center'> ALL RIGHTS RESERVED <br /> by Sushil yadav </div>
+        </div>
+    )
+}
+
+export default Footer
